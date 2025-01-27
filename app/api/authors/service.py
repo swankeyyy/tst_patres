df6e31afce74d5ed7938a98034d5d67a44fe9b40
levchuk <swankyyy1@gmail.com>
2025-01-27 16:15:57 +0300

    add put endpoint for update authors by id

--- app/api/authors/service.py
+++ app/api/authors/service.py
@@ -1,8 +1,9 @@
+from sqlalchemy import select
 from .schemas import AuthorCreate
 from app.src.models import Author
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from fastapi import HTTPException
 
 
@@ -22,3 +23,25 @@ class AuthorService:
         except IntegrityError as e:
             await session.rollback()
             raise HTTPException(status_code=409, detail="Author already exists")
+
+    @staticmethod
+    async def update_author(
+        author_id: str, author_update: AuthorCreate, session: AsyncSession
+    ) -> Author | Exception:
+        """Update author. Takes author_id and AuthorCreate object, returns updated author or raises 404 if author not found"""
+
+        try:
+            stmt = select(Author).filter(Author.id == author_id)
+            author = await session.execute(stmt)
+            author = author.scalars().first()
+
+            for key, value in author_update.model_dump(exclude_unset=True).items():
+                setattr(author, key, value)
+            session.add(author)
+            await session.commit()
+            await session.refresh(author)
+            return author
+        except SQLAlchemyError:
+            raise HTTPException(
+                status_code=404, detail="Author not found or wrong id length"
+            )
--- app/api/authors/views.py
+++ app/api/authors/views.py
@@ -6,7 +6,14 @@ from app.src.models.db_config import db_config
 router = APIRouter()
 
 
-@router.post("add_author/", status_code=status.HTTP_201_CREATED, response_model=Author)
+@router.post("add_author/", status_code=status.HTTP_201_CREATED, response_model=Author, summary="Add a new author")
 async def add_author(author: AuthorCreate, session=Depends(db_config.get_session)):
-    result = await AuthorService.create_author(author, session)
-    return result
+    """Add a new author with fields"""
+    author = await AuthorService.create_author(author, session)
+    return author
+
+@router.put("update_author/{author_id}", status_code=status.HTTP_200_OK, response_model=Author, summary="Update an author")
+async def update_author(author_id: str, author: AuthorCreate, session=Depends(db_config.get_session)):
+    """Update an author with fields"""
+    author = await AuthorService.update_author(author_id, author, session)
+    return author

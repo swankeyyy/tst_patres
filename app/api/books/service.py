e942f49d26f5afcf826da25451ef53483de52237
levchuk <swankyyy1@gmail.com>
2025-01-27 22:28:48 +0300

    add endpoint for updating book by id

--- app/api/books/service.py
+++ app/api/books/service.py
@@ -1,22 +1,55 @@
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload, joinedload
+
 from .schemas import BookCreate
 from app.src.models import Book
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException
-
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 
 class BookService:
-    
+
     @staticmethod
-    async def create_book(new_book: BookCreate, session: AsyncSession) -> Book | Exception:
+    async def create_book(
+        new_book: BookCreate, session: AsyncSession
+    ) -> Book | Exception:
         """Create new book by taked title and author UUID if the book with the same title and author UUID doesn't exist"""
-        stmt = select(Book).filter(Book.title == new_book.title).filter(Book.book_author_id == new_book.book_author_id)
-        book = await session.execute(stmt)
-        if book.scalars().first():
-            raise HTTPException(status_code=409, detail="Book already exists")
-        book = Book(**new_book.model_dump())
-        session.add(book)
-        await session.commit()
-        await session.refresh(book)
-        return book
+        try:
+            stmt = (
+                select(Book)
+                .filter(Book.title == new_book.title)
+                .filter(Book.book_author_id == new_book.book_author_id)
+            )
+            book = await session.execute(stmt)
+            if book.scalars().first():
+                raise HTTPException(status_code=409, detail="Book already exists")
+            book = Book(**new_book.model_dump())
+            session.add(book)
+            await session.commit()
+            await session.refresh(book)
+            return book
+        except IntegrityError:
+            await session.rollback()
+            raise HTTPException(status_code=400, detail="Invalid author id")
+
+    @staticmethod
+    async def update_book(
+        book_id: str, new_book: BookCreate, session: AsyncSession
+    ) -> Book | Exception:
+        """Update book by taked data and book UUID"""
+        try:
+            stmt = select(Book).filter(Book.id == book_id)
+            book = await session.execute(stmt)
+            book = book.scalars().first()
+
+            for key, value in new_book.model_dump(exclude_unset=True).items():
+                setattr(book, key, value)
+            session.add(book)
+            await session.commit()
+            await session.refresh(book)
+            return book
+        except SQLAlchemyError:
+            raise HTTPException(
+                status_code=404, detail="Wrong book id length or book not found"
+            )
--- app/api/books/views.py
+++ app/api/books/views.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter, Depends
+
+from typing import Union
+
 from .service import BookService
 from app.src.models.db_config import db_config
-from .schemas import BookCreate
+from .schemas import BookCreate, Book
 
 router = APIRouter()
 
-@router.post("/add/", summary="Create a new book")
-async def add_book(new_book: BookCreate, session=Depends(db_config.get_session)):
+@router.post("/add/", summary="Create a new book", response_model=Union[Book, None])
+async def add_book(new_book: BookCreate, session=Depends(db_config.get_session)) -> Book | Exception:
     """Create a new book by taked data and author UUID"""
     book = await BookService.create_book(new_book, session)
+    return book
+
+@router.put("/update/{book_id}", summary="Update book", response_model=Union[Book, None])
+async def update_book(book_id: str, new_book: BookCreate, session=Depends(db_config.get_session)) -> Book | Exception:
+    """Update book by taked data and book UUID"""
+    book = await BookService.update_book(book_id, new_book, session)
     return book

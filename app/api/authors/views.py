04b7b56981ba90693d7ece5ee845a71eab69a603
levchuk <swankyyy1@gmail.com>
2025-01-27 16:37:05 +0300

    add dependecy of current user on author endpoints

--- app/api/authors/views.py
+++ app/api/authors/views.py
@@ -3,6 +3,7 @@ from .schemas import AuthorCreate, Author
 from .service import AuthorService
 from app.src.models.db_config import db_config
 from typing import Union
+from app.api.dependencies import get_current_user
 
 router = APIRouter()
 
@@ -13,7 +14,7 @@ router = APIRouter()
     response_model=Author,
     summary="Add a new author",
 )
-async def add_author(author: AuthorCreate, session=Depends(db_config.get_session)):
+async def add_author(author: AuthorCreate, session=Depends(db_config.get_session), user=Depends(get_current_user)):
     """Add a new author with fields"""
     author = await AuthorService.create_author(author, session)
     return author
@@ -26,7 +27,7 @@ async def add_author(author: AuthorCreate, session=Depends(db_config.get_session
     summary="Update an author",
 )
 async def update_author(
-    author_id: str, author: AuthorCreate, session=Depends(db_config.get_session)
+    author_id: str, author: AuthorCreate, session=Depends(db_config.get_session), user=Depends(get_current_user)
 ):
     """Update an author with fields"""
     author = await AuthorService.update_author(author_id, author, session)
@@ -50,7 +51,7 @@ async def get_author(author_id: str, session=Depends(db_config.get_session)):
     status_code=status.HTTP_204_NO_CONTENT,
     summary="Delete an author by id",
 )
-async def delete_author(author_id: str, session=Depends(db_config.get_session)):
+async def delete_author(author_id: str, session=Depends(db_config.get_session), user=Depends(get_current_user)):
     """Delete an author by id"""
     await AuthorService.delete_author(author_id, session)
     return None
